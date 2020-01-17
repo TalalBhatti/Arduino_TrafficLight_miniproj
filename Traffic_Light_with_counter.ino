bc1c8ab984a23e573a880cc8c1d3e82aeafe695d
TalalBhatti <talalabhatti@gmail.com>
2020-01-17 23:21:32 +0500

    With counter

---
+++ Traffic_Light_with_counter.ino
@@ -0,0 +1,506 @@
+const int greenLED = 10;
+const int yellowLED = 9;
+const int redLED = 8;
+const int segA = 1;
+const int segB = 2;
+const int segC = 3;
+const int segD = 4;
+const int segE = 5;
+const int segF = 6;
+const int segG = 7;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode (greenLED, OUTPUT);
+pinMode (yellowLED, OUTPUT);
+pinMode (redLED, OUTPUT);
+pinMode (segA, OUTPUT);
+pinMode (segB, OUTPUT);
+pinMode (segC, OUTPUT);
+pinMode (segD, OUTPUT);
+pinMode (segE, OUTPUT);
+pinMode (segF, OUTPUT);
+pinMode (segG, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite (greenLED, LOW);
+digitalWrite (yellowLED, HIGH);
+digitalWrite (redLED, LOW);
+delay (2000);
+
+digitalWrite (segA, HIGH);
+digitalWrite (segB, LOW);
+digitalWrite (segC, LOW);
+digitalWrite (segD, LOW);
+digitalWrite (segE, LOW);
+digitalWrite (segF, LOW);
+digitalWrite (segG, LOW);
+delay (1000);
+
+digitalWrite (segA, HIGH);
+digitalWrite (segB, HIGH);
+digitalWrite (segC, LOW);
+digitalWrite (segD, HIGH);
+digitalWrite (segE, HIGH);
+digitalWrite (segF, HIGH);
+digitalWrite (segG, LOW);
+delay (1000);
+
+digitalWrite (segA, LOW);
+digitalWrite (segB, HIGH);
+digitalWrite (segC, LOW);
+digitalWrite (segD, LOW);
+digitalWrite (segE, LOW);
+digitalWrite (segF, LOW);
+digitalWrite (segG, HIGH);
+delay (1000);
+
+digitalWrite (segA, LOW);
+digitalWrite (segB, HIGH);
+digitalWrite (segC, LOW);
+digitalWrite (segD, LOW);
+digitalWrite (segE, HIGH);
+digitalWrite (segF, LOW);
+digitalWrite (segG, LOW);
+delay (1000);
+
+digitalWrite (segA, LOW);
+digitalWrite (segB, LOW);
+digitalWrite (segC, HIGH);
+digitalWrite (segD, LOW);
+digitalWrite (segE, HIGH);
+digitalWrite (segF, HIGH);
+digitalWrite (segG, LOW);
+delay (1000);
+
+digitalWrite (segA, LOW);
+digitalWrite (segB, LOW);
+digitalWrite (segC, LOW);
+digitalWrite (segD, HIGH);
+digitalWrite (segE, HIGH);
+digitalWrite (segF, LOW);
+digitalWrite (segG, LOW);
+delay (1000);
+
+digitalWrite (segA, LOW);
+digitalWrite (segB, LOW);
+digitalWrite (segC, LOW);
+digitalWrite (segD, HIGH);
+digitalWrite (segE, LOW);
+digitalWrite (segF, LOW);
+digitalWrite (segG, LOW);
+delay (1000);
+
+digitalWrite (segA, HIGH);
+digitalWrite (segB, HIGH);
+digitalWrite (segC, LOW);
+digitalWrite (segD, LOW);
+digitalWrite (segE, HIGH);
+digitalWrite (segF, HIGH);
+digitalWrite (segG, LOW);
+delay (1000);
+
+digitalWrite (segA, LOW);
+digitalWrite (segB, LOW);
+digitalWrite (segC, LOW);
+digitalWrite (segD, LOW);
+digitalWrite (segE, LOW);
+digitalWrite (segF, LOW);
+digitalWrite (segG, LOW);
+delay (1000);
+
+digitalWrite (segA, LOW);
+digitalWrite (segB, LOW);
+digitalWrite (segC, LOW);
+digitalWrite (segD, LOW);
+digitalWrite (segE, HIGH);
+digitalWrite (segF, HIGH);
+digitalWrite (segG, LOW);
+delay (1000);
+
+
+digitalWrite (greenLED, HIGH);
+digitalWrite (yellowLED, LOW);
+digitalWrite (redLED, LOW);
+delay (2000);
+
+digitalWrite (segA, HIGH);
+digitalWrite (segB, LOW);
+digitalWrite (segC, LOW);
+digitalWrite (segD, LOW);
+digitalWrite (segE, LOW);
+digitalWrite (segF, LOW);
+digitalWrite (segG, LOW);
+delay (1000);
+
+digitalWrite (segA, HIGH);
+digitalWrite (segB, HIGH);
+digitalWrite (segC, LOW);
+digitalWrite (segD, HIGH);
+digitalWrite (segE, HIGH);
+digitalWrite (segF, HIGH);
+digitalWrite (segG, LOW);
+delay (1000);
+
+digitalWrite (segA, LOW);
+digitalWrite (segB, HIGH);
+digitalWrite (segC, LOW);
+digitalWrite (segD, LOW);
+digitalWrite (segE, LOW);
+digitalWrite (segF, LOW);
+digitalWrite (segG, HIGH);
+delay (1000);
+
+digitalWrite (segA, LOW);
+digitalWrite (segB, HIGH);
+digitalWrite (segC, LOW);
+digitalWrite (segD, LOW);
+digitalWrite (segE, HIGH);
+digitalWrite (segF, LOW);
+digitalWrite (segG, LOW);
+delay (1000);
+
+digitalWrite (segA, LOW);
+digitalWrite (segB, LOW);
+digitalWrite (segC, HIGH);
+digitalWrite (segD, LOW);
+digitalWrite (segE, HIGH);
+digitalWrite (segF, HIGH);
+digitalWrite (segG, LOW);
+delay (1000);
+
+digitalWrite (segA, LOW);
+digitalWrite (segB, LOW);
+digitalWrite (segC, LOW);
+digitalWrite (segD, HIGH);
+digitalWrite (segE, HIGH);
+digitalWrite (segF, LOW);
+digitalWrite (segG, LOW);
+delay (1000);
+
+digitalWrite (segA, LOW);
+digitalWrite (segB, LOW);
+digitalWrite (segC, LOW);
+digitalWrite (segD, HIGH);
+digitalWrite (segE, LOW);
+digitalWrite (segF, LOW);
+digitalWrite (segG, LOW);
+delay (1000);
+
+digitalWrite (segA, HIGH);
+digitalWrite (segB, HIGH);
+digitalWrite (segC, LOW);
+digitalWrite (segD, LOW);
+digitalWrite (segE, HIGH);
+digitalWrite (segF, HIGH);
+digitalWrite (segG, LOW);
+delay (1000);
+
+digitalWrite (segA, LOW);
+digitalWrite (segB, LOW);
+digitalWrite (segC, LOW);
+digitalWrite (segD, LOW);
+digitalWrite (segE, LOW);
+digitalWrite (segF, LOW);
+digitalWrite (segG, LOW);
+delay (1000);
+
+digitalWrite (segA, LOW);
+digitalWrite (segB, LOW);
+digitalWrite (segC, LOW);
+digitalWrite (segD, LOW);
+digitalWrite (segE, HIGH);
+digitalWrite (segF, HIGH);
+digitalWrite (segG, LOW);
+delay (1000);
+
+digitalWrite (greenLED, LOW);
+digitalWrite (yellowLED, HIGH);
+digitalWrite (redLED, LOW);
+delay (2000);
+
+digitalWrite (segA, HIGH);
+digitalWrite (segB, LOW);
+digitalWrite (segC, LOW);
+digitalWrite (segD, LOW);
+digitalWrite (segE, LOW);
+digitalWrite (segF, LOW);
+digitalWrite (segG, LOW);
+delay (1000);
+
+digitalWrite (segA, HIGH);
+digitalWrite (segB, HIGH);
+digitalWrite (segC, LOW);
+digitalWrite (segD, HIGH);
+digitalWrite (segE, HIGH);
+digitalWrite (segF, HIGH);
+digitalWrite (segG, LOW);
+delay (1000);
+
+digitalWrite (segA, LOW);
+digitalWrite (segB, HIGH);
+digitalWrite (segC, LOW);
+digitalWrite (segD, LOW);
+digitalWrite (segE, LOW);
+digitalWrite (segF, LOW);
+digitalWrite (segG, HIGH);
+delay (1000);
+
+digitalWrite (segA, LOW);
+digitalWrite (segB, HIGH);
+digitalWrite (segC, LOW);
+digitalWrite (segD, LOW);
+digitalWrite (segE, HIGH);
+digitalWrite (segF, LOW);
+digitalWrite (segG, LOW);
+delay (1000);
+
+digitalWrite (segA, LOW);
+digitalWrite (segB, LOW);
+digitalWrite (segC, HIGH);
+digitalWrite (segD, LOW);
+digitalWrite (segE, HIGH);
+digitalWrite (segF, HIGH);
+digitalWrite (segG, LOW);
+delay (1000);
+
+digitalWrite (segA, LOW);
+digitalWrite (segB, LOW);
+digitalWrite (segC, LOW);
+digitalWrite (segD, HIGH);
+digitalWrite (segE, HIGH);
+digitalWrite (segF, LOW);
+digitalWrite (segG, LOW);
+delay (1000);
+
+digitalWrite (segA, LOW);
+digitalWrite (segB, LOW);
+digitalWrite (segC, LOW);
+digitalWrite (segD, HIGH);
+digitalWrite (segE, LOW);
+digitalWrite (segF, LOW);
+digitalWrite (segG, LOW);
+delay (1000);
+
+digitalWrite (segA, HIGH);
+digitalWrite (segB, HIGH);
+digitalWrite (segC, LOW);
+digitalWrite (segD, LOW);
+digitalWrite (segE, HIGH);
+digitalWrite (segF, HIGH);
+digitalWrite (segG, LOW);
+delay (1000);
+
+digitalWrite (segA, LOW);
+digitalWrite (segB, LOW);
+digitalWrite (segC, LOW);
+digitalWrite (segD, LOW);
+digitalWrite (segE, LOW);
+digitalWrite (segF, LOW);
+digitalWrite (segG, LOW);
+delay (1000);
+
+digitalWrite (segA, LOW);
+digitalWrite (segB, LOW);
+digitalWrite (segC, LOW);
+digitalWrite (segD, LOW);
+digitalWrite (segE, HIGH);
+digitalWrite (segF, HIGH);
+digitalWrite (segG, LOW);
+delay (1000);
+
+digitalWrite (greenLED, LOW);
+digitalWrite (yellowLED, LOW);
+digitalWrite (redLED, HIGH);
+delay (2000);
+
+
+digitalWrite (segA, HIGH);
+digitalWrite (segB, LOW);
+digitalWrite (segC, LOW);
+digitalWrite (segD, LOW);
+digitalWrite (segE, LOW);
+digitalWrite (segF, LOW);
+digitalWrite (segG, LOW);
+delay (1000);
+
+digitalWrite (segA, HIGH);
+digitalWrite (segB, HIGH);
+digitalWrite (segC, LOW);
+digitalWrite (segD, HIGH);
+digitalWrite (segE, HIGH);
+digitalWrite (segF, HIGH);
+digitalWrite (segG, LOW);
+delay (1000);
+
+digitalWrite (segA, LOW);
+digitalWrite (segB, HIGH);
+digitalWrite (segC, LOW);
+digitalWrite (segD, LOW);
+digitalWrite (segE, LOW);
+digitalWrite (segF, LOW);
+digitalWrite (segG, HIGH);
+delay (1000);
+
+digitalWrite (segA, LOW);
+digitalWrite (segB, HIGH);
+digitalWrite (segC, LOW);
+digitalWrite (segD, LOW);
+digitalWrite (segE, HIGH);
+digitalWrite (segF, LOW);
+digitalWrite (segG, LOW);
+delay (1000);
+
+digitalWrite (segA, LOW);
+digitalWrite (segB, LOW);
+digitalWrite (segC, HIGH);
+digitalWrite (segD, LOW);
+digitalWrite (segE, HIGH);
+digitalWrite (segF, HIGH);
+digitalWrite (segG, LOW);
+delay (1000);
+
+digitalWrite (segA, LOW);
+digitalWrite (segB, LOW);
+digitalWrite (segC, LOW);
+digitalWrite (segD, HIGH);
+digitalWrite (segE, HIGH);
+digitalWrite (segF, LOW);
+digitalWrite (segG, LOW);
+delay (1000);
+
+digitalWrite (segA, LOW);
+digitalWrite (segB, LOW);
+digitalWrite (segC, LOW);
+digitalWrite (segD, HIGH);
+digitalWrite (segE, LOW);
+digitalWrite (segF, LOW);
+digitalWrite (segG, LOW);
+delay (1000);
+
+digitalWrite (segA, HIGH);
+digitalWrite (segB, HIGH);
+digitalWrite (segC, LOW);
+digitalWrite (segD, LOW);
+digitalWrite (segE, HIGH);
+digitalWrite (segF, HIGH);
+digitalWrite (segG, LOW);
+delay (1000);
+
+digitalWrite (segA, LOW);
+digitalWrite (segB, LOW);
+digitalWrite (segC, LOW);
+digitalWrite (segD, LOW);
+digitalWrite (segE, LOW);
+digitalWrite (segF, LOW);
+digitalWrite (segG, LOW);
+delay (1000);
+
+digitalWrite (segA, LOW);
+digitalWrite (segB, LOW);
+digitalWrite (segC, LOW);
+digitalWrite (segD, LOW);
+digitalWrite (segE, HIGH);
+digitalWrite (segF, HIGH);
+digitalWrite (segG, LOW);
+delay (1000);
+
+digitalWrite (greenLED, LOW);
+digitalWrite (yellowLED, HIGH);
+digitalWrite (redLED, LOW);
+delay (2000);
+
+
+digitalWrite (segA, HIGH);
+digitalWrite (segB, LOW);
+digitalWrite (segC, LOW);
+digitalWrite (segD, LOW);
+digitalWrite (segE, LOW);
+digitalWrite (segF, LOW);
+digitalWrite (segG, LOW);
+delay (1000);
+
+digitalWrite (segA, HIGH);
+digitalWrite (segB, HIGH);
+digitalWrite (segC, LOW);
+digitalWrite (segD, HIGH);
+digitalWrite (segE, HIGH);
+digitalWrite (segF, HIGH);
+digitalWrite (segG, LOW);
+delay (1000);
+
+digitalWrite (segA, LOW);
+digitalWrite (segB, HIGH);
+digitalWrite (segC, LOW);
+digitalWrite (segD, LOW);
+digitalWrite (segE, LOW);
+digitalWrite (segF, LOW);
+digitalWrite (segG, HIGH);
+delay (1000);
+
+digitalWrite (segA, LOW);
+digitalWrite (segB, HIGH);
+digitalWrite (segC, LOW);
+digitalWrite (segD, LOW);
+digitalWrite (segE, HIGH);
+digitalWrite (segF, LOW);
+digitalWrite (segG, LOW);
+delay (1000);
+
+digitalWrite (segA, LOW);
+digitalWrite (segB, LOW);
+digitalWrite (segC, HIGH);
+digitalWrite (segD, LOW);
+digitalWrite (segE, HIGH);
+digitalWrite (segF, HIGH);
+digitalWrite (segG, LOW);
+delay (1000);
+
+digitalWrite (segA, LOW);
+digitalWrite (segB, LOW);
+digitalWrite (segC, LOW);
+digitalWrite (segD, HIGH);
+digitalWrite (segE, HIGH);
+digitalWrite (segF, LOW);
+digitalWrite (segG, LOW);
+delay (1000);
+
+digitalWrite (segA, LOW);
+digitalWrite (segB, LOW);
+digitalWrite (segC, LOW);
+digitalWrite (segD, HIGH);
+digitalWrite (segE, LOW);
+digitalWrite (segF, LOW);
+digitalWrite (segG, LOW);
+delay (1000);
+
+digitalWrite (segA, HIGH);
+digitalWrite (segB, HIGH);
+digitalWrite (segC, LOW);
+digitalWrite (segD, LOW);
+digitalWrite (segE, HIGH);
+digitalWrite (segF, HIGH);
+digitalWrite (segG, LOW);
+delay (1000);
+
+digitalWrite (segA, LOW);
+digitalWrite (segB, LOW);
+digitalWrite (segC, LOW);
+digitalWrite (segD, LOW);
+digitalWrite (segE, LOW);
+digitalWrite (segF, LOW);
+digitalWrite (segG, LOW);
+delay (1000);
+
+digitalWrite (segA, LOW);
+digitalWrite (segB, LOW);
+digitalWrite (segC, LOW);
+digitalWrite (segD, LOW);
+digitalWrite (segE, HIGH);
+digitalWrite (segF, HIGH);
+digitalWrite (segG, LOW);
+delay (1000);
+
+}
